eb40d0a96142ff55728fd32d749b8ed04bf70b11
prabhuganeshan <prabhuganeshang@gmail.com>
2020-08-24 12:58:12 +0530

    push my basic code

---
+++ GitTest/src/com/gittest/Gittest.java
@@ -0,0 +1,16 @@
+package com.gittest;
+
+public class Gittest {
+	public static void main(String[] args) {
+		System.out.println("main method");
+		System.out.println("after adding saranaa1");
+		System.out.println("after adding saranaa1");
+		System.out.println("after adding saranaa1");
+		System.out.println("after adding saranaa1");
+		System.out.println("after adding saranaa1");
+		System.out.println("after adding saranaa1");
+		System.out.println("after adding saranaa1");
+		
+		
+	}
+}
